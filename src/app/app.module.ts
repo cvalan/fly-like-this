b6e7f4812dd03d4a88244b11169e7839d1e9c441
cvalan <connorvalan@gmail.com>
2018-09-23 14:48:47 -0400

    added components for pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { NavigatonComponent } from './navigaton/navigaton.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReferencesComponent } from './references/references.component';
+import { ValuesComponent } from './values/values.component';
+import { TrainingComponent } from './training/training.component';
+import { GearComponent } from './gear/gear.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigatonComponent,
     FooterComponent,
-    HomepageComponent
+    HomepageComponent,
+    AboutComponent,
+    ContactComponent,
+    ReferencesComponent,
+    ValuesComponent,
+    TrainingComponent,
+    GearComponent
   ],
   imports: [
     BrowserModule,
